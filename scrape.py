ca71114dc4981f4b8e7a9f20181b41e78a841349
Nick <narivera1997@gmail.com>
2019-08-13 13:33:58 -0400

    request soc API

--- scrape.py
+++ scrape.py
@@ -3,16 +3,20 @@ Created on Aug 13, 2019
 
 @author: Nick
 '''
-
+import requests 
+#import grequests
+#from pip._vendor.requests.api import request
 
 
 if __name__ == '__main__':
     pass
 
-
-
 def main():
 
-    print("hello")
+    URL = "https://sis.rutgers.edu/oldsoc/courses.json?subject=198&semester=92019&campus=NB&level=U,G"
+    r = requests.get(URL)
+    data = r.json()
     
+    print(data)
+
 main()
